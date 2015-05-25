2af685bb604a6ce213b4aa709b64915416816a19
Pellissier <thibaudpellissier@gmail.com>
2015-05-25 19:56:41 +0200

    AJout des items pour les listes de vidéos

--- mobile/src/main/java/org/ahooper/MainActivity.java
+++ mobile/src/main/java/org/ahooper/MainActivity.java
@@ -1,17 +1,35 @@
 package org.ahooper;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends Activity {
 
+    private String[] drawerItemsList;
+    private ListView myDrawer;
+    private DrawerLayout mDrawerobj;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionbar = getActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeButtonEnabled(true);
         setContentView(R.layout.activity_main);
+
+        drawerItemsList = getResources().getStringArray(R.array.items);
+        myDrawer = (ListView) findViewById(R.id.my_drawer);
+        myDrawer.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_item, drawerItemsList));
+
     }
 }
